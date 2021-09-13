5a618a9c41f590b2833531d38d500aa74704dc02
Glitch (kalidoface3d) <none>
2021-09-13 03:08:27 +0000

    🎓💳 Updated with Glitch

--- src/utils/faceMap/helpers.js
+++
@@ -1,8 +0,0 @@
-export function clamp(value, min, max) {
-  return Math.max(Math.min(value, max), min);
-}
-
-export const remap = (val,a,b) => {
- //returns a to b -> 0 to 1 
- return (clamp(val,a,b) - a)/(b-a)
-}
